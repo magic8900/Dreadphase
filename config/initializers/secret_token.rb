3a594aecb96ddff2ded71f2f35b05011651367f6
Magic <magic8900@gmail.com>
2012-09-11 13:02:58 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dreadphase::Application.config.secret_token = 'd745bb8ad5dcd0bffa12e4ba3d2b984cf144d776c089918159330864b96c1c83c73010da334e70444a8f1013f637cc71689e126615d1ba48ee27b9c0fe104e22'
